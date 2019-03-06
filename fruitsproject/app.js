0ac962ef28083c811d366a5223f55e788f6b7afb
Noe Alvarado <44933742+CeroDosUno@users.noreply.github.com>
2019-03-06 14:04:22 -0800

    added embedded schemas into previous document. fruits are now embedded into person schema.

--- fruitsproject/app.js
+++ fruitsproject/app.js
@@ -24,13 +24,13 @@ const fruitSchema = new mongoose.Schema({
 const Fruit = mongoose.model("Fruit", fruitSchema);
 
 //creating an instance based on the model
-const fruit = new Fruit({
+const apple = new Fruit({
   name: "apple",
   rating : 7,
   review :"A solid fruit."
 });
 
-// fruit.save();
+apple.save();
 
 //save a bulk amount of new fruits
 //always reference the documentation
@@ -63,18 +63,38 @@ Fruit.insertMany([kiwi,orange,banana],function(err){
 
 //finding a fruit
 //only log their names
-Fruit.find(function(err,  fruits){
-  if(err){
-    console.log(err);
-  }
-  else{
-    mongoose.connection.close();
-    fruits.forEach(function(elem){
-          console.log(elem.name);
-    });
-  }
-});
+// Fruit.find(function(err,  fruits){
+//   if(err){
+//     console.log(err);
+//   }
+//   else{
+//     mongoose.connection.close();
+//     fruits.forEach(function(elem){
+//           console.log(elem.name);
+//     });
+//   }
+// });
 
+//updating a record
+// Fruit.updateOne({
+//  _id: "5c803a24d58278738a35ee19"
+// }, {name :"peach"}, function(err){
+//   if(err){
+//     console.log(err);
+//   }
+//   else{
+//     console.log('updated id');
+//   }
+// });
+//
+// Fruit.deleteOne({_id: "5c803a24d58278738a35ee19"}, function(err){
+//   if(err){
+//     console.log(err);
+//   }
+//   else{
+//     console.log('updated id');
+//   }
+// });
 
 //create person Schema
 //name and Age
@@ -83,16 +103,59 @@ Fruit.find(function(err,  fruits){
 //save it to db
 const personSchema = new mongoose.Schema({
   name: String,
-  age: Number
+  age: Number,
+  favoriteFruit: fruitSchema
 });
 
 const Person = mongoose.model("Person",personSchema);
 
+const pineapple = new Fruit({
+  name:"Pineapple",
+  score: 9,
+  review : "great fruit"
+});
+
+pineapple.save();
+
 const person = new Person({
   name: "John",
-  age : 24
+  age : 23,
 });
+
+// const person = new Person({
+//   name: "Amy",
+//   age : 12,
+//   favoritefruit : pineapple
+// });
 
 // person.save();
 
+Person.updateOne({name:"John"}, {favoriteFruit: apple}, function(err){
+  if(err){
+    console.log(err);
+  }
+  else{
+    console.log('updated johns favorite fruit');
+  }
+});
+
+Person.updateOne({name:"Amy"}, {favoriteFruit: pineapple}, function(err){
+  if(err){
+    console.log(err);
+  }
+  else{
+    console.log('updated amys favorite fruit');
+  }
+});
+
+
+//delete all people who are named John
+// Person.deleteMany({name: "John"}, function(err){
+//   if(err){
+//     console.log(err);
+//   }
+//   else{
+//     console.log('deleted john');
+//   }
+// });
 //EOF
